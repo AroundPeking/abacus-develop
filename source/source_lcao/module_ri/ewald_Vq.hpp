0fba0b6041e0605f842c565518f61d2c0ab2cb36
Yuyang ji <1041176461@qq.com>
2025-06-06 22:41:14 +0800

    fix ewald param

--- source/source_lcao/module_ri/ewald_Vq.hpp
+++ source/source_lcao/module_ri/ewald_Vq.hpp
@@ -785,27 +785,23 @@ auto Ewald_Vq<Tdata>::set_Vq_dVq(const UnitCell& ucell,
                         for (size_t n1 = 0; n1 != this->g_abfs[it1][l1].size();
                              ++n1) {
                             const double pB = this->multipole[it1][l1][n1];
-                            Tin_convert pp
-                                = RI::Global_Func::convert<Tin_convert>(pA
-                                                                        * pB);
-                            for (size_t m0 = 0; m0 != 2 * l0 + 1; ++m0) {
-                                const size_t index0
-                                    = this->index_abfs[it0][l0][n0][m0];
+                            Tin_convert frac = RI::Global_Func::convert<Tin_convert>(pA * pB * this->info.hybrid_alpha);
+                            for (size_t m0 = 0; m0 != 2 * l0 + 1; ++m0)
+                            {
+                                const size_t index0 = this->index_abfs[it0][l0][n0][m0];
                                 const size_t lm0 = l0 * l0 + m0;
                                 for (size_t m1 = 0; m1 != 2 * l1 + 1; ++m1) {
                                     const size_t index1
                                         = this->index_abfs[it1][l1][n1][m1];
                                     const size_t lm1 = l1 * l1 + m1;
 
-                                    LRI_CV_Tools::add_elem(
-                                        data,
-                                        index0,
-                                        index1,
-                                        Vq_dVq_gauss[list_A0_pair_k[i0]]
-                                                    [list_A1_pair_k[i1]],
-                                        lm0,
-                                        lm1,
-                                        pp);
+                                    LRI_CV_Tools::add_elem(data,
+                                                           index0,
+                                                           index1,
+                                                           Vq_dVq_gauss[list_A0_pair_k[i0]][list_A1_pair_k[i1]],
+                                                           lm0,
+                                                           lm1,
+                                                           frac);
                                 }
                             }
                         }
