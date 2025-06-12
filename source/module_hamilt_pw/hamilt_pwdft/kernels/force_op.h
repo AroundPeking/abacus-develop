5c9680229facc75057c9ec6a14fd4273753a320a
dyzheng <zhengdy@bjaisi.com>
2025-06-12 12:00:46 +0800

    fix: force error of CUDA

--- source/module_hamilt_pw/hamilt_pwdft/kernels/force_op.h
+++ source/module_hamilt_pw/hamilt_pwdft/kernels/force_op.h
@@ -226,6 +226,7 @@ struct cal_force_nl_op<FPTYPE, base_device::DEVICE_GPU>
                     const int& nbands,
                     const int& ik,
                     const int& nkb,
+                    const int& npol,
                     const int* atom_nh,
                     const int* atom_na,
                     const FPTYPE& tpiba,
