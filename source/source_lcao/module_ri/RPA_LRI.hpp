945f5c6ed858f9adec43cb6ef9393d09cf757f11
Huanjing Gong <108850865+AroundPeking@users.noreply.github.com>
2025-06-25 14:34:13 +0800

    fix: cal atom position in RPA
    feat: use malloc_trim to release memory

--- source/source_lcao/module_ri/RPA_LRI.hpp
+++ source/source_lcao/module_ri/RPA_LRI.hpp
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <malloc.h>
 #include <vector>
 
 using TA = int;
@@ -69,7 +70,7 @@ void RPA_LRI<T, Tdata>::cal_large_Cs(const LCAO_Orbitals& orb, const K_Vectors& 
         = RI::Distribute_Equally::distribute_atoms_periods(this->mpi_comm, atoms, period_Cs, 2, false);
     std::map<TA, TatomR> atoms_pos;
     for (int iat = 0; iat < ucell.nat; ++iat)
-        atoms_pos[iat] = RI_Util::Vector3_to_array3(ucell.atoms[iat].tau[ucell.iat2ia[iat]]);
+        atoms_pos[iat] = RI_Util::Vector3_to_array3(ucell.atoms[ucell.iat2it[iat]].tau[ucell.iat2ia[iat]]);
     const std::array<TatomR, Ndim> latvec = {RI_Util::Vector3_to_array3(ucell.a1),
                                              RI_Util::Vector3_to_array3(ucell.a2),
                                              RI_Util::Vector3_to_array3(ucell.a3)};
@@ -100,6 +101,7 @@ void RPA_LRI<T, Tdata>::cal_large_Cs(const LCAO_Orbitals& orb, const K_Vectors& 
     Cs_period.swap(tmp);
     delete exx_lri_rpa;
     exx_lri_rpa = nullptr;
+    malloc_trim(0);
 
     ModuleBase::timer::tick("RPA_LRI", "cal_large_Cs");
 }
@@ -284,6 +286,7 @@ void RPA_LRI<T, Tdata>::out_for_RPA(const UnitCell& ucell,
               << (pelec->f_en.etot - pelec->f_en.etxc + exx_lri_rpa->Eexx) / 2.0 << std::endl;
     delete exx_lri_rpa;
     exx_lri_rpa = nullptr;
+    malloc_trim(0);
 
     if (GlobalC::exx_info.info_ri.shrink_abfs_pca_thr >= 0.0)
     {
@@ -324,6 +327,7 @@ void RPA_LRI<T, Tdata>::out_for_RPA(const UnitCell& ucell,
 
         delete exx_full_coulomb;
         exx_full_coulomb = nullptr;
+        malloc_trim(0);
     }
 
     return;
