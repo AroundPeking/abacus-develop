a506bb5f208ec33a670f7ab7fbf28f76276cb2d0
AroundPeking <gonghuanjing@iphy.ac.cn>
2026-05-22 22:47:17 +0800

    Add tunable Ewald Gaussian lambda

--- source/source_lcao/module_ri/exx_lri.hpp
+++ source/source_lcao/module_ri/exx_lri.hpp
@@ -867,10 +867,11 @@ void Exx_LRI<Tdata>::init(const MPI_Comm &mpi_comm_in,
 															this->abfs_old_to_new_per_type );
 				if (settings_list.first == Conv_Coulomb_Pot_K::Coulomb_Method::Ewald)
 				{
-					this->exx_objs[settings_list.first].evq.init(ucell, orb,
-																this->mpi_comm, this->p_kv, this->lcaos, this->abfs,
-																settings_list.second.second, this->MGT, this->info.ccp_rmesh_times, this->info.kmesh_times,
-																this->abfs_old_to_new_per_type);
+						this->exx_objs[settings_list.first].evq.init(ucell, orb,
+																	this->mpi_comm, this->p_kv, this->lcaos, this->abfs,
+																	settings_list.second.second, this->MGT, this->info.ccp_rmesh_times,
+																	this->info.ewald_lambda, this->info.kmesh_times,
+																	this->abfs_old_to_new_per_type);
 				}
 			}
 
@@ -953,10 +954,11 @@ void Exx_LRI<Tdata>::init(const MPI_Comm &mpi_comm_in,
 															this->abfs_old_to_new_per_type );
 				if (settings_list.first == Conv_Coulomb_Pot_K::Coulomb_Method::Ewald)
 				{
-					this->exx_objs[settings_list.first].evq.init(ucell, orb,
-																this->mpi_comm, this->p_kv, this->lcaos, this->abfs,
-																settings_list.second.second, this->MGT, this->info.ccp_rmesh_times, this->info.kmesh_times,
-																this->abfs_old_to_new_per_type);
+						this->exx_objs[settings_list.first].evq.init(ucell, orb,
+																	this->mpi_comm, this->p_kv, this->lcaos, this->abfs,
+																	settings_list.second.second, this->MGT, this->info.ccp_rmesh_times,
+																	this->info.ewald_lambda, this->info.kmesh_times,
+																	this->abfs_old_to_new_per_type);
 				}
 			}
 
